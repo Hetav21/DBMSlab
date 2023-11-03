59d30ba9ec324b3d868f95d56be70afc779bdd41
Hetav21 <shahhetav2106@gmail.com>
2023-11-03 11:30:32 +0530

    Adding Practical 11

--- lab_11.sql
+++ lab_11.sql
@@ -3,51 +3,80 @@ CREATE DATABASE IF NOT EXISTS HETAV;
 USE HETAV;
 
 CREATE TABLE IF NOT EXISTS c4(
-    id INT PRIMARY KEY,
-    enrollment_no INT UNIQUE
+    Roll_No INT PRIMARY KEY,
+    Stdudent_Name VARCHAR(50) NOT NULL,
+    Enrollment_No INT UNIQUE,
+    Batch VARCHAR(50),
+    Age INT
 );
-
-DESC c4;
 
 INSERT INTO c4
 VALUES
-(1,  220179),
-(2,  220128),
-(3,  220189),
-(4,  220274),
-(5,  220253),
-(6,  220292);
-
+(1, "Hetav Shah", 220179, "c4", 19),
+(2, "Varun Thacker", 220176, "c4", 17),
+(3, "Aditya Singh", 220189, "c4", 18);
 SELECT * FROM c4;
 
-CREATE TABLE IF NOT EXISTS marks(
-    id INT PRIMARY KEY,
-    cgpa INT NOT NULL
-);
+DELIMITER //
 
-DESC marks;
+CREATE PROCEDURE Retrieve_Students_Age()
+BEGIN
+    -- Step 1: Declare variables
+    DECLARE student_name VARCHAR(30);
+    DECLARE student_age INT;
 
-INSERT INTO marks
+    DECLARE done INT DEFAULT FALSE;
+    -- Step 2: Declare a cursor to select students over 19 years old
+    DECLARE student_cursor CURSOR FOR
+        SELECT Stdudent_Name, Age
+        FROM c4
+        WHERE Roll_No = 1;
+
+    -- Handle exceptions if the cursor doesn't exist
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    -- Step 3: Open the cursor
+    OPEN student_cursor;
+
+    read_loop: LOOP
+        -- Step 3 (continued): Fetch data from the cursor
+        FETCH student_cursor INTO student_name, student_age;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        -- Step 4: Process the retrieved data as needed (you can print or use the data here)
+        SELECT CONCAT(student_name, ' has age = ', student_age);
+    END LOOP;
+
+    -- Step 5: Close the cursor
+    CLOSE student_cursor;
+
+END;
+//
+
+-- To call the cursor procedure
+DELIMITER ;
+CALL Retrieve_Students_Age();
+
+DELIMITER //
+-- Step 6: Deallocate the cursor (optional)
+DEALLOCATE PREPARE student_cursor;
+//
+
+DELIMITER //
+CREATE TRIGGER update_batch
+BEFORE INSERT ON c4
+FOR EACH ROW
+BEGIN
+    -- Set the Batch to 'C1' for new student inserts
+    SET NEW.Batch = 'C1';
+END;
+//
+
+DELIMITER ;
+INSERT INTO c4
 VALUES
-(1, 6),
-(2, 7),
-(3, 8),
-(4, 3),
-(5, 9),
-(6, 8),
-(7, 4),
-(8, 8);
-
-SELECT * FROM marks;
-
-DECLARE xyz CURSOR FOR SELECT * FROM c4;
-
-OPEN xyz;
-
-
-
-CLOSE xyz;
-
-DEALLOCATE xyz;
+(4, "Riken Prajapati", 220169, "c2" ,18);
+SELECT * FROM c4;
 
 DROP DATABASE IF EXISTS HETAV;
