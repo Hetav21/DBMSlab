0a995a767a74e1df16bc13b987f7a151e78d8362
Hetav21 <shahhetav2106@gmail.com>
2023-11-18 14:30:48 +0530

    Updating Practical 12.3

--- lab_12.3.sql
+++ lab_12.3.sql
@@ -40,13 +40,9 @@ VALUES
 ("105", "keyur", "dadar", 7500, 20061119),
 ("106", "mayur", "borivali", 5500, 20061221);
 
-/* 1 */
 SELECT * FROM Deposit;
 SELECT * FROM Job;
 SELECT * FROM Employee;
-
-
-
 
 
 
