8347120fae279f173b1f7b9b8df3f01cf5ac830f
Hetav21 <shahhetav2106@gmail.com>
2023-11-18 15:30:17 +0530

    Adding File for Practical 12.4

--- lab_12.4.sql
+++ lab_12.4.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS c4;
 
 USE c4;
 
+CREATE TABLE Job (job_id VARCHAR(15) ,job_title VARCHAR(30) , min_sal DECIMAL(7,2) , max_sal DECIMAL(7,2));
+
 CREATE TABLE Employee (emp_no DECIMAL(3) ,emp_name VARCHAR(30) ,emp_sal DECIMAL(8,2) , emp_comm DECIMAL(6, 1), dept_no DECIMAL(3));
 
 INSERT INTO Employee
@@ -13,6 +15,39 @@ VALUES
 (105, "Anita", 5000, 50000, 10),
 (106, "Sneha", 2450, 24500, 10),
 (107, "Anamika", 2975, NULL, 30);
+
+INSERT INTO Job
+VALUES
+("IT_PROG", "Programmer", 4000, 10000),
+("MK_MGR", "Marketing manager", 9000, 15000),
+("FI_MGR", "Finance manager", 8200, 12000),
+("FI_ACC", "Account", 4200, 9000),
+("LEC", "Lecturer", 6000, 17000),
+("COMP_OP", "Computer Operator", 1500, 3000);
+
+
+/* 1 */
+SELECT CURRENT_DATE as Date ;
+
+/* 2 */
+
+
+/* 3 */
+
+
+/* 4 */
+
+
+/* 5 */
+
+
+/* 6 */
+
+
+/* 7 */
+
+
+/* 8 */
 
 
 
